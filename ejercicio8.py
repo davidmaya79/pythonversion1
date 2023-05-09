38cd37f7aab2c2839650261e24e58cec94d49da8
davidmaya79 <davidmaya79@gmail.com>
2023-05-09 17:11:04 -0400

    Se Agrega el comit

---
+++ ejercicio8.py
@@ -0,0 +1,13 @@
+num1 = int(input("Ingrese el primer numero :"))
+num2 = int(input("Ingrese el segundo numero :"))
+num3 = int(input("Ingrese el tercer numero :"))
+num4 = int(input("Ingrese el cuarto numero :"))
+
+suma = num1 + num2 + num3 + num4
+promedio = suma / 4
+
+print("La suma de los numeros es :")
+print(suma)
+
+print("El producto de los numeros es :")
+print(promedio)
---
+++ ejercicio9.py
@@ -0,0 +1,7 @@
+cantidad_horas = int(input("Ingrese las horas trabajadas :"))
+valor_horas = int(input("Ingrese el valor de las horas :"))
+
+sueldo = cantidad_horas * valor_horas
+
+print("El sueldo correspondiente es :")
+print(sueldo)
