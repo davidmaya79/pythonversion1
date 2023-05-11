efe61f3c826dd8c61c3b06869d842d2f3b9b8223
davidmaya79 <davidmaya79@gmail.com>
2023-05-11 16:10:37 -0400

    Se Agrega el comit

---
+++ ejercicio12.py
@@ -0,0 +1,17 @@
+num1 = int(input("Ingrese el primer numero :"))
+num2 = int(input("Ingrese el segundo numero :"))
+
+if num1 > num2:
+    suma = num1 + num2
+    resta = num1 - num2
+    print("La suma de los numero es :")
+    print(suma)
+    print("La resta de los numeros es :")
+    print(resta)
+else:
+    division = num1 / num2
+    multiplicacion = num1 *num2
+    print("La division de los numeros es :")
+    print(division)
+    print("La multiplicacion de los numeros es :")
+    print(multiplicacion)
---
+++ ejercicio13.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingrese la primera nota :"))
+num2 = int(input("Ingrese la segunda nota :"))
+num3 = int(input("Ingrese la tercera nota :"))
+
+promedio = (num1 + num2 + num3) / 3
+
+if promedio >= 7:
+    print("Promocionado")
